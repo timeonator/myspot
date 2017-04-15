236f5ed8d3ca131b8eb03a07135be57bb95b4d97
Robert Bruce Gifford <timeonator@gmail.com>
2017-04-14 18:59:35 -0700

    Update app.component.ts

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { ProductService } from './product.service';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+@Component({selector: 'app-root',
+template: `<h1>{{title}}</h1>
+<products></products>`,
+providers: [ProductService]
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'MySpot';
 }
