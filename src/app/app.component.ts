f010ba14acbd87538ba6b745b91dfe0c6ae7c8d5
Robert Gifford <timeonator@gmail.com>
2017-04-17 12:42:53 -0700

    commiting work in progress on spotify api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,42 @@ import { Observable } from 'rxjs/Rx';
 import { SpotifyService } from './spotify.service';
 
 
-@Component({selector: 'app-root',
-template: ``,
+@Component({
+selector: 'app-root',
+styles:[`
+  .img {
+    position: relative;
+    float: left;
+    width: 100px;
+    height: 100px;
+    background-position:50% 50%;
+    background-repeat: no-repeat;
+    background-size: cover;
+  }
+  `],
+template: `
+<div *ngIf="isLoading">Getting data...</div>
+<div *ngFor="let item of artists.items" class="media">
+  <div class="media-left">
+    <a href="#">
+      <img class="media-object-img" src="{{item.images[0]?.url}}" alt="...">
+    </a>
+  </div>
+  <div class="media-body">
+    <h4 class="media-heading">Media heading</h4>
+    
+  </div>
+</div>
+`,
 providers: [SpotifyService]
 })
 export class AppComponent {
+  isLoading = true;
+  artists = [];
   constructor(private _spotifyService:SpotifyService){
-    this._spotifyService.getSpotifyData().subscribe(data => console.log(data));
+  }
+  ngOnInit(){
+    this._spotifyService.getSpotifyData()
+    .subscribe( data => {this.isLoading = false; this.artists = data.artists; console.log(this.artists); });
   }
 }
